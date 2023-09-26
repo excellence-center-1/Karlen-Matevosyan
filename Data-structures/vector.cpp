eb1b91bc11464a5774ee1af535f57a40765a16f9
Karlen <karlen.matevosyan.ec@gmail.com>
2023-09-26 23:56:22 +0400

    Added insert,erase,operator[],at,front and back methods

--- Data-structures/vector.cpp
+++ Data-structures/vector.cpp
@@ -19,7 +19,7 @@ class Vector {
             delete [] data;
         }   
 
-        void push(const b1t& value){
+        void push_back(const b1t& value){
             if (current == capacity){
 
                 capacity *= 2;
@@ -33,6 +33,74 @@ class Vector {
             data[current++] = value;
         }
 
+
+        void insert(size_t pos, const b1t& value) {
+            if (current == capacity) {
+                capacity *= 2;
+                b1t* temp = new b1t[capacity];
+
+                for (size_t i = 0; i < pos; i++) {
+                    temp[i] = data[i];
+                }
+                temp[pos] = value;
+
+                for (size_t i = pos; i < current; i++) {
+                    temp[i + 1] = data[i];
+                }
+
+                delete[] data;
+                data = temp;
+            } else {
+                for (size_t i = current; i > pos; i--) {
+                    data[i] = data[i - 1];
+                }
+                data[pos] = value;
+            }
+            ++current;
+        }
+
+        void erase(size_t position) {
+            for (size_t i = position; i < current - 1; i++) {
+                data[i] = data[i + 1];
+            }
+            --current;
+        }
+
+        void clear() {
+            current = 0;
+        }
+
+        b1t& operator[](size_t index) {
+            if (index >= current) {
+                throw out_of_range("Index out of range");
+            }
+            return data[index];
+        }
+
+        b1t& at(size_t index) {
+            if (index >= current) {
+                throw out_of_range("Index out of range");
+            }
+            return data[index];
+        }  
+
+
+        b1t& front() {
+            if (current == 0) {
+                throw out_of_range("Vector is empty");
+            }
+            return data[0];
+        }
+
+        b1t& back() {
+            if (current == 0) {
+                throw out_of_range("Vector is empty");
+            }
+            return data[current - 1];
+        }  
+
+
+
         size_t getSize() const {
             return current;
         }
@@ -42,25 +110,50 @@ class Vector {
         }
 
         void print() const {
-            cout<<"The Elements: [ ";
+            cout <<"The Elements: [ ";
             for (size_t i = 0; i < current; ++i) {
-                cout<<data[i]<<" ";
+                cout << data[i] << " ";
             }
-            cout<<"]";
+            cout << "]";
         }
 };
 
 int main () {
     Vector <int> v;
 
-    for (int i = 1; i <=5; i++){
-        v.push(i);
-    }
+    v.push_back(17);
+    v.push_back(6);
+    v.push_back(9);
+    v.push_back(30);
+    v.push_back(14);
 
     v.print();
 
     cout << "\nSize: " << v.getSize() << "\n";
     cout << "Capacity: " << v.getCapacity() << "\n";
+
+    cout << "Front: " << v.front() << endl;
+    cout << "Back: " << v.back() << endl;    
+
+    v.insert(2, 100);
+    cout << "After inserting 100 at index 2: [";
+    for (size_t i = 0; i < v.getSize(); ++i) {
+        cout << " " << v[i];
+    }
+    cout <<" ]";
+    cout << endl;
+
+    v.erase(3);
+    cout << "After erasing element at index 3: [";
+    for (size_t i = 0; i < v.getSize(); ++i) {
+        cout << " " << v[i];
+    }
+    cout <<" ]";
+    cout << endl;
+
+    v.clear();
+    cout << "After clearing the vector, size: " << v.getSize() << endl;
+
 
     return 0;
 }
